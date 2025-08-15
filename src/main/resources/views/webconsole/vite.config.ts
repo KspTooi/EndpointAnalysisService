e4f04674cde432291e0059ba9ba9604d0293a859
KspTooi <ksptooi@outlook.com>
2025-08-16 00:49:21 +0800

    feat:配置Vite以处理DTS自动生成 配置Iconify的图标自动生成DTS

--- src/main/resources/views/webconsole/vite.config.ts
+++ src/main/resources/views/webconsole/vite.config.ts
@@ -1,9 +1,10 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-// import monacoEditorPlugin from 'vite-plugin-monaco-editor';
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import Components from "unplugin-vue-components/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,39 +14,46 @@ export default defineConfig({
     // (monacoEditorPlugin as any).default({
     //   languageWorkers: ['editorWorkerService', 'css', 'html', 'json', 'typescript']
     // }),
+    Components({
+      resolvers: [IconsResolver()],
+    }),
+    icons({
+      autoInstall: true,
+      compiler: "vue3",
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 27501,
     cors: {
-      origin: '*',
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-    }
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    },
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
     rollupOptions: {
       input: {
-        admin: fileURLToPath(new URL('./admin-entry.html', import.meta.url))
+        admin: fileURLToPath(new URL("./admin-entry.html", import.meta.url)),
       },
       output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
         assetFileNames: (assetInfo) => {
-          const name = assetInfo.name || '';
-          if (name.endsWith('.css')) {
-            return 'assets/[name].css';
+          const name = assetInfo.name || "";
+          if (name.endsWith(".css")) {
+            return "assets/[name].css";
           }
-          return 'assets/[name].[ext]';
-        }
-      }
-    }
-  }
-})
+          return "assets/[name].[ext]";
+        },
+      },
+    },
+  },
+});
