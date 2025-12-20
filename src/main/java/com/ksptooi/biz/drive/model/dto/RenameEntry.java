b513047c7cdbc0782373a49875f167c09b05fa85
KspTooi <ksptooi@outlook.com>
2025-12-21 06:55:07 +0800

    fix:1.3P CP1 云盘->重命名功能 将@Range替换为@Size，@Range用于数值校验，@Size用于字符串长度校验。

--- src/main/java/com/ksptooi/biz/drive/model/dto/RenameEntry.java
+++ src/main/java/com/ksptooi/biz/drive/model/dto/RenameEntry.java
@@ -1,9 +1,9 @@
 package com.ksptooi.biz.drive.model.dto;
 
-import org.hibernate.validator.constraints.Range;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,7 +17,7 @@ public class RenameEntry {
 
     @Schema(description = "条目名称")
     @NotBlank(message = "条目名称不能为空")
-    @Range(min = 1, max = 128, message = "条目名称长度必须在1-128个字符之间")
+    @Size(min = 1, max = 128, message = "条目名称长度必须在1-128个字符之间")
     private String name;
 
 }
