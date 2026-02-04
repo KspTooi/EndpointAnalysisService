a3ecaf58d0e02bcc3bf3ac2a5b8edb9dd0b0dac3
KspTooi <ksptooi@outlook.com>
2026-02-04 18:11:03 +0800

    feat: 1.5L CP1 代码生成器更换为支持VM模板且兼容Hibernate7.0的Assembly系列组件

---
+++ src/main/resources/RunDatabase.java
@@ -0,0 +1,32 @@
+package com.ksptooi;
+
+
+
+import com.ksptooi.commons.H2Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.sql.SQLException;
+
+public class RunDatabase {
+
+    public static void main(String[] args) throws SQLException {
+
+        var port = 59000;
+
+        if (!isPortInUse(port)) {
+            H2Server h2Server = new H2Server(59000);
+            h2Server.start();
+        }
+
+    }
+    public static boolean isPortInUse(int port) {
+        try {
+            new ServerSocket(port).close();
+            return false;
+        } catch (IOException e) {
+            return true;
+        }
+    }
+
+}
