0de6eaec91810e6a8ba00f4c349fe4f9a8a92413
KspTooi <ksptooi@outlook.com>
2026-02-04 10:21:09 +0800

    feat:1.5H CP1 更新版本号

--- src/main/resources/views/webconsole/src/soa/console-framework/ConsoleFramework.vue
+++ src/main/resources/views/webconsole/src/soa/console-framework/ConsoleFramework.vue
@@ -7,7 +7,7 @@
         :items="menuTree"
         :active-item-id="activeMenuId"
         title="EAS服务管理控制台"
-        version="版本:1.5G CP8"
+        version="版本:1.5H CP1"
         @item-click="handleMenuClick"
         @action="handleMenuAction"
       />
