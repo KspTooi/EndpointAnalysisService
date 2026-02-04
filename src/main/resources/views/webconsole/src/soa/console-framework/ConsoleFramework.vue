9f2a2c12b0a5314527232476c0047e57e42efd94
KspTooi <ksptooi@outlook.com>
2026-02-04 14:26:51 +0800

    feat: 1.5K CP2 版本号同步

--- src/main/resources/views/webconsole/src/soa/console-framework/ConsoleFramework.vue
+++ src/main/resources/views/webconsole/src/soa/console-framework/ConsoleFramework.vue
@@ -7,7 +7,7 @@
         :items="menuTree"
         :active-item-id="activeMenuId"
         title="EAS服务管理控制台"
-        version="版本:1.5J CP9"
+        version="版本:1.5K CP2"
         @item-click="handleMenuClick"
         @action="handleMenuAction"
       />
