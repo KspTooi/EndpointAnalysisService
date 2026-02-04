f32d6af1198730667c6dc7185e655d2507d2c1ed
KspTooi <ksptooi@outlook.com>
2026-02-04 13:36:39 +0800

    fix:1.5J CP9 中继请求记录 修复默认查询时间问题

--- src/main/java/com/ksptooi/biz/relay/controller/RequestController.java
+++ src/main/java/com/ksptooi/biz/relay/controller/RequestController.java
@@ -45,9 +45,9 @@ public class RequestController {
             }
         }
 
-        //如果不填写开始时间结束时间 则以今天的00:00:00开始 向前推15天
+        //如果不填写开始时间结束时间 则以今天的23:59:59开始 向前推15天
         if(dto.getStartTime() == null || dto.getEndTime() == null){
-            LocalDateTime startOfToday = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
+            LocalDateTime startOfToday = LocalDateTime.now().withHour(23).withMinute(59).withSecond(59).withNano(999999999);
             LocalDateTime fifteenDaysAgo = startOfToday.minusDays(15);
             dto.setStartTime(fifteenDaysAgo);
             dto.setEndTime(startOfToday);
