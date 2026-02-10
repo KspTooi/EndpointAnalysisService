13402f26604228671e253de3e637089a6e3a22ca
KspTooi <ksptooi@outlook.com>
2026-02-10 12:42:48 +0800

    feat: QT定时任务 同步PO

--- src/main/java/com/ksptooi/biz/qt/model/qttask/QtTaskPo.java
+++ src/main/java/com/ksptooi/biz/qt/model/qttask/QtTaskPo.java
@@ -5,7 +5,6 @@ import com.ksptool.assembly.entity.exception.AuthException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
