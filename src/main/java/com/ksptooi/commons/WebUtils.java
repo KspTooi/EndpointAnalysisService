1af603e714ca688aeaa87b81fa9d4a5986b495e6
KspTooi <ksptooi@outlook.com>
2026-02-12 09:19:32 +0800

    feat: 添加获取IP地址的方法，优化WebUtils类，调整注释格式以提高可读性

--- src/main/java/com/ksptooi/commons/WebUtils.java
+++ src/main/java/com/ksptooi/commons/WebUtils.java
@@ -1,21 +1,21 @@
 package com.ksptooi.commons;
 
-import org.apache.commons.lang3.StringUtils;
-
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.StringUtils;
 
 public class WebUtils {
 
     /**
      * 获取Cookie值
-     * @param request 请求
-     * @param cookieName  Cookie名称
+     *
+     * @param request    请求
+     * @param cookieName Cookie名称
      * @return Cookie值 如果Cookie不存在，则返回null
      */
     public static String getCookieValue(HttpServletRequest request, String cookieName) {
         var cookies = request.getCookies();
-        if (cookies!= null) {
-            for (var cookie: cookies) {
+        if (cookies != null) {
+            for (var cookie : cookies) {
                 if (cookie.getName().equals(cookieName)) {
                     return cookie.getValue();
                 }
@@ -26,6 +26,7 @@ public class WebUtils {
 
     /**
      * 获取Authentication Bearer Token中的sessionId
+     *
      * @param request 请求
      * @return Authentication Bearer Token中的sessionId 不存在或格式不正确，则返回null
      */
@@ -50,6 +51,25 @@ public class WebUtils {
         return sessionId;
     }
 
+    /*
+     * 获取IP地址
+     * @param request 请求
+     * @return IP地址
+     */
+    private String getIpAddr(HttpServletRequest request) {
+
+        if (request == null) {
+            return "unknown";
+        }
+
+        String ip = request.getHeader("X-REAL-IP");
+
+        if (StringUtils.isNotBlank(ip)) {
+            return ip;
+        }
+
+        return request.getRemoteAddr();
+    }
 
 
 }
