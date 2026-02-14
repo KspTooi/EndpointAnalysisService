5aad09de7f5c680d2ed54d41877e41f9cab3c714
KspTooi <ksptooi@outlook.com>
2026-02-14 18:24:16 +0800

    refactor: 添加权限验证注解并更新Swagger注解以增强PostController的方法安全性和文档化，确保只有具备相应权限的用户可以访问岗位管理功能

--- src/main/java/com/ksptooi/biz/post/controller/PostController.java
+++ src/main/java/com/ksptooi/biz/post/controller/PostController.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 import com.ksptooi.biz.post.service.PostService;
 import com.ksptooi.biz.post.model.dto.AddPostDto;
 import com.ksptooi.biz.post.model.dto.EditPostDto;
@@ -20,36 +20,41 @@ import com.ksptooi.biz.post.model.vo.GetPostListVo;
 import com.ksptooi.biz.post.model.vo.GetPostDetailsVo;
 
 
+@PrintLog
 @RestController
 @RequestMapping("/post")
-@Tag(name = "post", description = "岗位表")
+@Tag(name = "岗位管理", description = "岗位管理")
 @Slf4j
 public class PostController {
 
     @Autowired
     private PostService postService;
 
+    @PreAuthorize("@auth.hasCode('core:post:view')")
     @PostMapping("/getPostList")
-    @Operation(summary ="列表查询")
+    @Operation(summary ="查询岗位列表")
     public PageResult<GetPostListVo> getPostList(@RequestBody @Valid GetPostListDto dto) throws Exception{
         return postService.getPostList(dto);
     }
 
-    @Operation(summary ="新增")
+    @PreAuthorize("@auth.hasCode('core:post:add')")
+    @Operation(summary ="新增岗位")
     @PostMapping("/addPost")
     public Result<String> addPost(@RequestBody @Valid AddPostDto dto) throws Exception{
 		postService.addPost(dto);
         return Result.success("新增成功");
     }
 
-    @Operation(summary ="编辑")
+    @PreAuthorize("@auth.hasCode('core:post:edit')")
+    @Operation(summary ="编辑岗位")
     @PostMapping("/editPost")
     public Result<String> editPost(@RequestBody @Valid EditPostDto dto) throws Exception{
 		postService.editPost(dto);
         return Result.success("修改成功");
     }
 
-    @Operation(summary ="查询详情")
+    @PreAuthorize("@auth.hasCode('core:post:view')")
+    @Operation(summary ="查询岗位详情")
     @PostMapping("/getPostDetails")
     public Result<GetPostDetailsVo> getPostDetails(@RequestBody @Valid CommonIdDto dto) throws Exception{
         GetPostDetailsVo details = postService.getPostDetails(dto);
@@ -59,7 +64,8 @@ public class PostController {
         return Result.success(details);
     }
 
-    @Operation(summary ="删除")
+    @PreAuthorize("@auth.hasCode('core:post:remove')")
+    @Operation(summary ="删除岗位")
     @PostMapping("/removePost")
     public Result<String> removePost(@RequestBody @Valid CommonIdDto dto) throws Exception{
         postService.removePost(dto);
