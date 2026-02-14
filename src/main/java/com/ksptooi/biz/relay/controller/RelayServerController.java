c57e242588b82a5c698c2a7a077628a49725c1b9
KspTooi <ksptooi@outlook.com>
2026-02-14 17:50:26 +0800

    refactor: 添加权限验证注解和Swagger注解以增强RelayServerController的方法安全性和文档化，确保只有具备相应权限的用户可以访问相关功能

--- src/main/java/com/ksptooi/biz/relay/controller/RelayServerController.java
+++ src/main/java/com/ksptooi/biz/relay/controller/RelayServerController.java
@@ -6,8 +6,14 @@ import com.ksptool.assembly.entity.exception.BizException;
 import com.ksptool.assembly.entity.web.CommonIdDto;
 import com.ksptool.assembly.entity.web.PageResult;
 import com.ksptool.assembly.entity.web.Result;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,32 +21,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/relayServer")
+@Tag(name = "relayServer", description = "中继服务器")
 public class RelayServerController {
 
 
     @Autowired
     private RelayServerService relayServerService;
 
-    /**
-     * 获取中继服务器列表
-     *
-     * @param dto 查询条件
-     * @return 中继服务器列表
-     */
+
+    @PreAuthorize("@auth.hasCode('relay:server:view')")
+    @Operation(summary = "获取中继服务器列表")
     @PostMapping("/getRelayServerList")
     public PageResult<GetRelayServerListVo> getRelayServerList(@RequestBody @Valid GetRelayServerListDto dto) {
         return relayServerService.getRelayServerList(dto);
     }
 
-    /**
-     * 添加中继服务器
-     *
-     * @param dto 中继服务器信息
-     * @throws BizException
-     */
+ 
+    @PreAuthorize("@auth.hasCode('relay:server:add')")
+    @Operation(summary = "添加中继服务器")
     @PostMapping("/addRelayServer")
     public Result<String> addRelayServer(@RequestBody @Valid AddRelayServerDto dto) throws BizException {
 
@@ -54,12 +55,9 @@ public class RelayServerController {
         return Result.success("添加中继服务器成功");
     }
 
-    /**
-     * 编辑中继服务器
-     *
-     * @param dto 中继服务器信息
-     * @throws BizException
-     */
+
+    @PreAuthorize("@auth.hasCode('relay:server:edit')")
+    @Operation(summary = "编辑中继服务器")
     @PostMapping("/editRelayServer")
     public Result<String> editRelayServer(@RequestBody @Valid EditRelayServerDto dto) throws BizException {
 
@@ -73,68 +71,44 @@ public class RelayServerController {
     }
 
 
-    /**
-     * 获取中继服务器详情
-     *
-     * @param dto 中继服务器ID
-     * @return 中继服务器详情
-     */
+    @PreAuthorize("@auth.hasCode('relay:server:view')")
+    @Operation(summary = "获取中继服务器详情")
     @PostMapping("/getRelayServerDetails")
     public Result<GetRelayServerDetailsVo> getRelayServerDetails(@RequestBody @Valid CommonIdDto dto) throws BizException {
         return Result.success(relayServerService.getRelayServerDetails(dto.getId()));
     }
 
-    /**
-     * 删除中继服务器
-     *
-     * @param dto 中继服务器ID
-     * @throws BizException
-     */
+    @PreAuthorize("@auth.hasCode('relay:server:remove')")
+    @Operation(summary = "删除中继服务器")
     @PostMapping("/removeRelayServer")
     public Result<String> removeRelayServer(@RequestBody @Valid CommonIdDto dto) throws BizException {
         relayServerService.removeRelayServer(dto.getId());
         return Result.success("删除中继服务器成功");
     }
 
-    /**
-     * 启动中继服务器
-     *
-     * @param dto 中继服务器ID
-     * @throws BizException
-     */
+    @PreAuthorize("@auth.hasCode('relay:server:edit')")
+    @Operation(summary = "启动中继服务器")
     @PostMapping("/startRelayServer")
     public Result<String> startRelayServer(@RequestBody @Valid CommonIdDto dto) throws BizException {
         return relayServerService.startRelayServer(dto.getId());
     }
 
-    /**
-     * 停止中继服务器
-     *
-     * @param dto 中继服务器ID
-     * @throws BizException
-     */
+    @PreAuthorize("@auth.hasCode('relay:server:edit')")
+    @Operation(summary = "停止中继服务器")
     @PostMapping("/stopRelayServer")
     public Result<String> stopRelayServer(@RequestBody @Valid CommonIdDto dto) throws BizException {
         return relayServerService.stopRelayServer(dto.getId());
     }
 
-    /**
-     * 获取中继服务器路由状态
-     *
-     * @param dto 中继服务器ID
-     * @throws BizException
-     */
+    @PreAuthorize("@auth.hasCode('relay:server:edit')")
+    @Operation(summary = "获取中继服务器路由状态")
     @PostMapping("/getRelayServerRouteState")
     public Result<List<GetRelayServerRouteStateVo>> getRelayServerRouteState(@RequestBody @Valid CommonIdDto dto) throws BizException {
         return Result.success(relayServerService.getRelayServerRouteState(dto.getId()));
     }
 
-    /**
-     * 重置熔断状态
-     *
-     * @param dto 中继服务器ID
-     * @throws BizException
-     */
+    @PreAuthorize("@auth.hasCode('relay:server:edit')")
+    @Operation(summary = "重置熔断状态")
     @PostMapping("/resetRelayServerBreaker")
     public Result<String> resetRelayServerBreaker(@RequestBody @Valid ResetRelayServerBreakerDto dto) throws BizException {
         relayServerService.resetRelayServerBreaker(dto);
