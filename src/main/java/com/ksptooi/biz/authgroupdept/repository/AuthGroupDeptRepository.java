a24b90d462cee94ecfc73c2a40a479b283228ccc
KspTooi <ksptooi@outlook.com>
2026-02-15 08:22:29 +0800

    refactor: 修改 AuthGroupDeptRepository 接口，更新主键类型并优化查询排序逻辑

--- src/main/java/com/ksptooi/biz/authgroupdept/repository/AuthGroupDeptRepository.java
+++ src/main/java/com/ksptooi/biz/authgroupdept/repository/AuthGroupDeptRepository.java
@@ -1,23 +1,23 @@
 package com.ksptooi.biz.authgroupdept.repository;
 
 import com.ksptooi.biz.authgroupdept.model.AuthGroupDeptPo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthGroupDeptRepository extends JpaRepository<AuthGroupDeptPo, Long>{
+public interface AuthGroupDeptRepository extends JpaRepository<AuthGroupDeptPo, AuthGroupDeptPo.Pk> {
 
     @Query("""
     SELECT u FROM AuthGroupDeptPo u
     WHERE
-    (:#{#po.groupId} IS NULL OR u.groupId  = :#{#po.groupId} )
-    AND (:#{#po.deptId} IS NULL OR u.deptId  = :#{#po.deptId} )
-    AND (:#{#po.createTime} IS NULL OR u.createTime  = :#{#po.createTime} )
-    ORDER BY u.updateTime DESC
+    (:#{#po.groupId} IS NULL OR u.groupId = :#{#po.groupId})
+    AND (:#{#po.deptId} IS NULL OR u.deptId = :#{#po.deptId})
+    AND (:#{#po.createTime} IS NULL OR u.createTime = :#{#po.createTime})
+    ORDER BY u.createTime DESC
     """)
     Page<AuthGroupDeptPo> getAuthGroupDeptList(@Param("po") AuthGroupDeptPo po, Pageable pageable);
 }
