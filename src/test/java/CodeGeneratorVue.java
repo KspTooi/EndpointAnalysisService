642a47b9d16643ded769997db2dddd8cc8e69184
KspTooi <ksptooi@outlook.com>
2026-02-23 11:05:22 +0800

    refactor: 删除favicon.ico文件并更新测试用例以使用新的表名zremoved_auth_group

--- src/test/java/CodeGeneratorVue.java
+++ src/test/java/CodeGeneratorVue.java
@@ -16,7 +16,7 @@ public class CodeGeneratorVue {
         var databaseName = "endpoint_analysis_service";
 
         //需要生成的
-        var tableNames = List.of("core_post");
+        var tableNames = List.of("zremoved_auth_group");
 
         //创建 MySQL 采集器
         MysqlCollector coll = new MysqlCollector();
