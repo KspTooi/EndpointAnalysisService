e9001111cd7e5900ede0bcc7567e752d23cb2017
KspTooi <ksptooi@outlook.com>
2026-02-24 22:24:59 +0800

    feat: 在RowScopePo类中添加了rootId、deptId和creatorId字段的@Transient注解，并更新了类注释以明确子类的字段声明要求

--- src/main/java/com/ksptool/bio/biz/auth/common/aop/RowScopePo.java
+++ src/main/java/com/ksptool/bio/biz/auth/common/aop/RowScopePo.java
@@ -2,6 +2,8 @@ package com.ksptool.bio.biz.auth.common.aop;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Transient;
+
 import org.hibernate.annotations.Filter;
 import org.hibernate.annotations.FilterDef;
 import org.hibernate.annotations.ParamDef;
@@ -18,8 +20,10 @@ import org.hibernate.annotations.ParamDef;
  * 5. 仅本人: rsMax = 4
  * 6. 指定部门: rsMax = 5
  * <p>
+ * <p>
  * 请注意: 尽管在这个类中已经预定义了三个数据权限所必须的参数，但根据编码规范，继承此类的子类还需要显式声明这些参数。
- *
+ * @apiNote 在子类中需要显式声明 rootId、deptId 和 creatorId 字段，否则在查询时会报错
+ * @author KspTool
  */
 @MappedSuperclass
 @FilterDef(name = "rsFilter", parameters = {
@@ -38,12 +42,15 @@ import org.hibernate.annotations.ParamDef;
 )
 public abstract class RowScopePo {
 
+    @Transient
     @Column(name = "root_id", nullable = false, updatable = false, comment = "所属企业(租户)ID")
     private Long rootId;
 
+    @Transient
     @Column(name = "dept_id", nullable = false, updatable = false, comment = "所属部门ID")
     private Long deptId;
 
+    @Transient
     @Column(name = "creator_id", nullable = false, updatable = false, comment = "创建者ID")
     private Long creatorId;
 
