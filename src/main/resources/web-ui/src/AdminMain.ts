e4e762f03b779950879fb6358d6458c6393bcd5c
KspTooi <ksptooi@outlook.com>
2026-03-21 16:06:56 +0800

    feat: 初始化Pinia并添加持久化插件，优化应用状态管理

--- src/main/resources/web-ui/src/AdminMain.ts
+++ src/main/resources/web-ui/src/AdminMain.ts
@@ -1,5 +1,6 @@
 import { createApp, markRaw } from "vue";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import AdminRoot from "@/AdminRoot.vue";
 // 导入 Element Plus
 import ElementPlus from "element-plus";
@@ -24,20 +25,25 @@ import AuditRouteRegister from "@/views/audit/route/AuditRouteRegister";
 import QtRouteRegister from "@/views/qt/route/QtRouteRegister.ts";
 import GenRouteRegister from "@/views/assembly/route/GenRouteRegister.ts";
 
+//初始化Iconify
 setupIconify();
 
+//创建应用实例
 const app = createApp(AdminRoot);
 
-// 注册所有图标并使用 markRaw 包装
+//注册所有图标并使用 markRaw 包装
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, markRaw(component));
 }
 
-app.use(createPinia());
+//注册Pinia 并加入持久化插件
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
 
 const { initialize, addRoute } = GenricRouteService.useGenricRoute();
 
-//注册业务路由
+//在SOA路由服务中注册域业务路由
 addRoute(new AuthRouteRegister());
 addRoute(new CoreRouteRegister());
 addRoute(new DriveRouteRegister());
@@ -48,11 +54,13 @@ addRoute(new AuditRouteRegister());
 addRoute(new QtRouteRegister());
 addRoute(new GenRouteRegister());
 
-//初始化路由服务
+//初始化SOA路由服务
 initialize(app);
 
 // 使用Element Plus并设置为中文
 app.use(ElementPlus, {
   locale: zhCn,
 });
+
+//挂载应用
 app.mount("#app");
