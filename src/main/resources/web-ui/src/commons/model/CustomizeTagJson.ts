05da32f5f1552c8b7fcef21db6784f5cb4b93520
KspTooi <ksptooi@outlook.com>
2026-03-23 06:41:04 +0800

    feat: 新增CustomizeTagJson和ExtendedFileAttachJson接口以支持自定义标签和扩展文件附件数据结构

---
+++ src/main/resources/web-ui/src/commons/model/CustomizeTagJson.ts
@@ -0,0 +1,6 @@
+/**
+ * 自定义标签JSON
+ */
+export default interface CustomizeTagJson {
+  n: string; // 标签名称
+}
---
+++ src/main/resources/web-ui/src/commons/model/ExtendedFileAttachJson.ts
@@ -0,0 +1,7 @@
+export default interface ExtendedFileAttachJson {
+  i: number; // 附件ID
+  p: string; // 附件路径
+  n: string; // 附件名称
+  t: string; // 附件类型
+  l: number; // 附件大小
+}
