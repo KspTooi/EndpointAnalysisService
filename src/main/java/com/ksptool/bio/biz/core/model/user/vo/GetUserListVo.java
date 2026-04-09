97e477f75221460015424aba9a0198236dd57cd2
KspTooi <ksptooi@outlook.com>
2026-04-09 18:03:45 +0800

    feat: 为GetUserListVo类添加无参构造函数，以支持对象的默认初始化

--- src/main/java/com/ksptool/bio/biz/core/model/user/vo/GetUserListVo.java
+++ src/main/java/com/ksptool/bio/biz/core/model/user/vo/GetUserListVo.java
@@ -3,6 +3,7 @@ package com.ksptool.bio.biz.core.model.user.vo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class GetUserListVo {
 
     @Schema(description = "用户ID")
