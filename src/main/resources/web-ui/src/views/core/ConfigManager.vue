43cd4f8fa61b4b46e00c619b3a3cc878644c050b
KspTooi <ksptooi@outlook.com>
2026-04-09 18:22:26 +0800

    fix: 为用户名称列添加格式化功能，以处理空值显示为'-'

--- src/main/resources/web-ui/src/views/core/ConfigManager.vue
+++ src/main/resources/web-ui/src/views/core/ConfigManager.vue
@@ -65,8 +65,9 @@
             placement: 'top',
             enterable: false,
           }"
+          :formatter="(_, __, value) => value ?? '-'"
         />
-        <el-table-column prop="userName" label="所有者" min-width="120" />
+        <el-table-column prop="userName" label="所有者" min-width="120" :formatter="(_, __, value) => value ?? '-'" />
         <el-table-column label="操作" fixed="right" min-width="140">
           <template #default="scope">
             <el-button link type="primary" size="small" :icon="EditIcon" @click="openModal('edit', scope.row)">
